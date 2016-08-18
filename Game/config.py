9d59ee812fda25a2732d277bf9f3cea3a09a9e0a
Charter Jacobson <me@katacarbix.xyz>
2016-08-18 17:14:30 -0500

    Switched to a better way of locating variables.ini

--- Game/config.py
+++ Game/config.py
@@ -4,18 +4,18 @@ from configparser import ConfigParser
 currDir = os.path.dirname(os.path.realpath(__file__))
 config = ConfigParser()
 
-def load_var(var_name, section='setup_vars', isInt=True, filename='\\variables.ini'):
-	config.read(currDir + filename)
+def load_var(var_name, section='setup_vars', isInt=True, filename='variables.ini'):
+	config.read(os.path.join(currDir, filename))
 	if(isInt):
 		return int(config.get(section, var_name))
 	return config.get(section, var_name)
 
-def save_var_string(section, var_name, var_string, filename='\\variables.ini'):
-	config.read(currDir + filename)
+def save_var_string(section, var_name, var_string, filename='variables.ini'):
+	config.read(os.path.join(currDir, filename))
 	if not config.has_section(section):
 		config.add_section(section)
 	config[section][var_name] = var_string
-	with open(currDir + filename, 'w') as configfile:
+	with open(os.path.join(currDir, filename), 'w') as configfile:
 		config.write(configfile)
 
 #Test code
